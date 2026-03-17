babf934778cee083a061b9581879f0ce22c54041
Manish KL <manishklach@gmail.com>
2026-03-17 14:18:48 -0700

    Updated Verilator

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,7 @@
 set -e
 
 echo "Running Verilator Lint..."
-verilator --lint-only -Wall -Wno-fatal \
+verilator --lint-only --timing -Wall -Wno-fatal \
     rtl/ric.sv rtl/hrm_region.sv rtl/srmesh_router.sv rtl/srmic_top.sv \
     tb/tb_top.sv
 
